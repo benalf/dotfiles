da291091385c361c1cbc6f0a207bc589fdd92094
Alfred Bendrup <me@alfredbendrup.me>
2023-09-22 10:57:48 +0300

    change: kitty and fish sitting in a tree

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,13 @@ end
 set fish_greeting
 
 export PATH="$HOME/.cargo/bin:$PATH"
+export EDITOR=nvim
+export MANPAGER='nvim +Man!'
+export KITTY_ENABLE_WAYLAND=1
 
+# opam configuration
+source /home/alf/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+# >>> coursier install directory >>>
+set -gx PATH "$PATH:/home/alf/.local/share/coursier/bin"
+# <<< coursier install directory <<<
