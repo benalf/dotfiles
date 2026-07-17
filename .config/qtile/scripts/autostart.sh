a3038a02e9ec0038bcd0cafb3ed3bf86ae96aac0
Alfred Bendrup <me@alfredbendrup.me>
2026-07-17 10:28:28 +0300

    new: kill old config

--- .config/qtile/scripts/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-picom --experimental-backends --config $HOME/.config/qtile/scripts/picom.conf &
-nitrogen --restore &
-nm-applet &
-eval `ssh-agent -s`
-ssh-add
-ibus-daemon &
-xset r rate 180 30
